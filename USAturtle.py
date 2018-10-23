235d7e19b1fd2b430e7e54242f44cf444f22cfae
Ouch <dearzhw@gmail.com>
2018-10-23 23:56:50 +0800

    stupid turtle

--- USAturtle.py
+++ USAturtle.py
@@ -2,5 +2,19 @@
 # TestForSelenium
 
 import turtle
+def walk():
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(100)
+
 
 wn = turtle.Screen()
+my_turtle =turtle.Turtle()
+my_turtle.shape("turtle")
+while True:
+    walk()
+turtle.mainloop()
